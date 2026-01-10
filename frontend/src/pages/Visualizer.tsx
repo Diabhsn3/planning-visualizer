cf4de9dcb0606956f0ddaf142de5eae3143345dd
Hasan Diab <hdiab16@gmail.com>
2026-01-10 13:02:09 +0200

    Update Visualizer.tsx

--- frontend/src/pages/Visualizer.tsx
+++ frontend/src/pages/Visualizer.tsx
@@ -150,31 +150,28 @@ export default function Visualizer() {
   )
 
   (:init
-      ;; truck starts at d1
       (at-truck t1 d1)
 
-      ;; cranes
       (at-crane c1 d1)
       (empty-crane c1)
       (at-crane c2 d2)
       (empty-crane c2)
 
-      ;; piles at depots
-      ;; p2 is buried under p1 at d1
+      (at-pile pile1 d1)
+      (at-pile pile2 d2)
+
+      (on p2 pile1)
       (on p1 p2)
-      (on-pile p2 pile1)
+
       (clear p1)
       (clear pile2)
-
-      ;; pile1 not clear because p2 is on it
-      ;; pile2 empty and clear
   )
 
-  ;; Goal: move p2 to d2 on pile2
   (:goal
-      (on-pile p2 pile2)
+      (on p2 pile2)
   )
 )
+
 `;
     }
 
